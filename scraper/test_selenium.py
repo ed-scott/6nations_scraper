5d62b00d732bdc251ae2d1d71596608450ee72fe
Ed Scott <ed.scott@clekt.co.uk>
2024-04-15 13:46:17 +0100

    Now able to login, now I need to navigate correctly to the correct pages using javascript.

--- scraper/test_selenium.py
+++ scraper/test_selenium.py
@@ -14,12 +14,27 @@ chromeOptions = uc.ChromeOptions()
 chromeOptions.headless = True
 driver = uc.Chrome(use_subprocess=True, options=chromeOptions) 
 driver.get(sign_in_url)
-time.sleep(10) # To let the login page load.
+time.sleep(3) # To let the login page load.
 
 # Send username and password into various boxes
 uname = driver.find_element(By.ID, ":R1ial9ltsrqla:") 
 uname.send_keys(email_input_id)
 pword = driver.find_element(By.ID, "password")
 pword.send_keys(password_input_id)
-driver.find_element(By.ID, ":R4ial9ltsrqla:").click() 
+button = driver.find_element(By.ID, ":R4ial9ltsrqla:")
+button.click()
+time.sleep(3)
+driver_logs = driver.get_log("driver")
+browser_logs = driver.get_log("browser")
+
+#find the button-play button and click it
+driver.get("https://fantasy.sixnationsrugby.com/")
+time.sleep(3)
+button_play = driver.find_element(By.XPATH("//button-play")).get_attribute("innerHTML")
+button_play.click()
+time.sleep(5)
+
+# Get the first match in question
+driver.get("https://fantasy.sixnationsrugby.com/m6n/#/game/play/me")
+
 time.sleep(5)
